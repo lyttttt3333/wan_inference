53443139adbd18fe717f64484f8eaa65d235761a
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-08-10 23:42:04 +0800

    .

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-python generate.py \
+torchrun --nproc_per_node=8 generate.py \
     --task ti2v-5B \
     --size 1280*704 \
     --ckpt_dir /lustre/fsw/portfolios/av/users/shiyil/jfxiao/DiffSynth-Studio/models/Wan-AI/Wan2.2-TI2V-5B \
